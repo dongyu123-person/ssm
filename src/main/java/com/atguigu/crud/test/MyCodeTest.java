aefa7645d525a027ff892c917cac30dbee6ba1fb
dongyu123-person <1349580592@qq.com>
2021-03-13 19:45:28 +0800

    替换工程

---
+++ src/main/java/com/atguigu/crud/test/MyCodeTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.crud.test;
+
+import org.junit.Test;
+
+public class MyCodeTest {
+
+    static {
+        System.out.println("12");
+    }
+
+    static {
+        System.out.println("23");
+    }
+
+    @Test
+    public void test(){
+        System.out.println("1");
+    }
+}
