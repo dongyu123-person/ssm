20e79559a91727550e64bffb8ddd0202c5436991
dongyu123-person <1349580592@qq.com>
2021-03-13 19:29:56 +0800

    第一次提交git

--- src/main/java/cn/itcast/controller/AccountController.java
+++ src/main/java/cn/itcast/controller/AccountController.java
@@ -3,15 +3,12 @@ package cn.itcast.controller;
 import cn.itcast.domain.Account;
 import cn.itcast.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -25,12 +22,12 @@ public class AccountController {
     public String findAll(Model model) {
         System.out.println("表现层：查询所有的账户。。。");
         List<Account> list = accountService.findAll();
-        model.addAttribute("list",list);
+        model.addAttribute("list", list);
         return "list";
     }
 
     @RequestMapping("/save")
-    public String save(Account account,HttpServletRequest request, HttpServletResponse response) {
+    public String save(Account account, HttpServletRequest request, HttpServletResponse response) {
         accountService.saveAccount(account);
         return "forward:/account/testFindAll";
 
@@ -41,7 +38,6 @@ public class AccountController {
 //        }
 //        return "list";
     }
-
 
 
 }
